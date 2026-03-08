ef5e211f4e65987c91e5064d041ee40e865a09f6
Basil_nc <basilncherian@gmail.com>
2026-03-08 14:44:03 -0700

    added questions

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,23 @@
 export const WORD_CATEGORIES: Record<string, string[]> = {
-  randoms: [
+  Questions: [
+    'Who among us is most likely to end up in jail for something completely accidental?', 
+    'If we were all in a horror movie, who would be the first person to get "deleted"?', 
+    'Who is most likely to start a cult by mistake because they’re just that persuasive?', 
+    'If someone here was a secret double agent, who would it be?', 
+    'Who is most likely to forget their own wedding anniversary?', 
+    'If we were stranded on an island, who would we definitely eat first?', 
+    'Who among us would try to haggle the price of a hospital bill?', 
+    'Who is most likely to accidentally join a gang while looking for a washroom?', 
+    'If someone here became a billionaire, who would pretend they don\'t know the rest of us?', 
+    'Who is most likely to get arrested for "disturbing the peace" at a library?', 
+  'Who would be the first to cry during a slightly stressful board game?', 
+  'If we were in a heist, who would be the one to trip the alarm by sneezing?', 
+  'Who among us has the most "villain origin story" energy?', 
+  'Who is most likely to get scammed by a fake prince on the internet?', 
+  'If we were all characters in a Malayalam movie, who would be the comic relief that dies for no reason?', 
+  'Who is most likely to go to the store for milk and come back with a pet goat?', 
+  'Who among us is the most likely to have a secret second family in another state?'
+],  randoms: [
     // Animals
     'Elephant', 'Giraffe', 'Penguin', 'Kangaroo', 'Lion', 'Tiger', 'Zebra', 'Dolphin', 'Shark', 'Panda', 'Koala', 'Hamster', 'Octopus', 'Eagle', 'Owl',
     // Movies
